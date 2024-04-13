858bcde8bf112c9718bb93ae466c08aee0062be2
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-13 14:21:41 +0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Java Spring Restful Starter template
-Template này được sử dụng cho series Java Spring Restful (dự án jobhunter) của tác giả Hỏi Dân IT (Eric)
+# Java Spring Starter template
+Template này được sử dụng cho series Java Spring Restful (dự án jobHunter) của tác giả Hỏi Dân IT (Eric)
 
 ## Về tác giả
 Mọi thông tin về Tác giả Hỏi Dân IT, các bạn có thể tìm kiếm tại đây:
--- src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
@@ -3,7 +3,13 @@ package vn.hoidanit.jobhunter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
+// @SpringBootApplication
 public class JobhunterApplication {
 
 	public static void main(String[] args) {
