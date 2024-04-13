07e28e543e515d02e9cc6ea78c3313638c69682d
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-13 11:19:25 +0700

    init project

---
+++ src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.jobhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhunterApplication.class, args);
+	}
+
+}
